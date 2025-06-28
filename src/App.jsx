f4c9492f38b18003a39e0118a7f66d886fb7680c
nayeem19999 <nhnayeem13@gmail.com>
2025-06-28 19:56:05 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
 import "./App.css";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      App Is Ready
-      <div>
-        {/* Open the modal using document.getElementById('ID').showModal() method */}
-        <button
-          className="btn"
-          onClick={() => document.getElementById("my_modal_1").showModal()}
-        >
-          Open modal this section is made by Ali
-        </button>
-        <dialog id="my_modal_1" className="modal">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">
-              Press ESC key or click the button below to close
-            </p>
-            <div className="modal-action">
-              <form method="dialog">
-                {/* if there is a button in form, it will close the modal */}
-                <button className="btn">Close</button>
-              </form>
-            </div>
-          </div>
-        </dialog>
-      </div>
+    <div>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
